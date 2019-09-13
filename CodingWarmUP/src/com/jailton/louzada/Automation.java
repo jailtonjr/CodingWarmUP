60fee0ee898e345fcac0d0389999ddf67042befb
jailtonjr <jailtonalkimin@gmail.com>
2019-09-13 14:29:14 +0200

    All done! https://codingbat.com/java/Logic-1

--- CodingWarmUP/src/com/jailton/louzada/Automation.java
+++ CodingWarmUP/src/com/jailton/louzada/Automation.java
@@ -1432,5 +1432,456 @@ Unless outsideMode is true, in which case return true if the number is less or e
 
 	}
 
+/*	Return true if the given non-negative number is a multiple of 3 or 5, but not both. 
+ * Use the % "mod" operator -- see Introduction to Mod
+
+
+	old35(3) → true
+	old35(10) → true
+	old35(15) → false
+	*/
+	public boolean old35(int n) {
+		if ((n % 3 == 0)) {
+			if ((n % 5 == 0)) {
+				return false;
+			}
+			return true;
+		}
+
+		if ((n % 5 == 0)) {
+			return true;
+		}
+		return false;
+	}
+
+
+/*Return true if the given non-negative number is 1 or 2 less than a multiple of 20. 
+So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+
+
+less20(18) → true
+less20(19) → true
+less20(20) → false*/
 	
+	public boolean less20(int n) {
+		int rest = n % 20;
+		if (rest >= 18 && rest <= 22) {
+			return true;
+		}
+		return false;
+	}
+
+
+/*Given a non-negative number "num", return true if num is within 2 of a multiple of 10. 
+Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+
+
+nearTen(12) → true
+nearTen(17) → false
+nearTen(19) → true*/
+
+	public boolean nearTen(int num) {
+		int rest = num % 10;
+		if (rest <=2  || rest >=8) {
+			return true;
+		}
+		return false;
+	}
+
+
+/*Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. 
+So if either value is a teen, just return 19.
+
+
+teenSum(3, 4) → 7
+teenSum(10, 13) → 19
+teenSum(13, 2) → 19*/
+
+	public int teenSum(int a, int b) {
+		if (a >= 13 && a <= 19) {
+			return 19;
+		}
+		
+		if (b >= 13 && b <= 19) {
+			return 19;
+		}
+		return a+b;
+	}
+
+	
+
+/*Your cell phone rings. Return true if you should answer it. 
+Normally you answer, except in the morning you only answer if it is your mom calling. 
+In all cases, if you are asleep, you do not answer.
+
+
+answerCell(false, false, false) → true
+answerCell(false, false, true) → false
+answerCell(true, false, false) → false*/
+	
+	public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+		if (isAsleep) {
+			return false;
+		}
+		
+		if (isMorning) {
+			if (isMom) {
+				return true;
+			}else return false;
+		}
+		
+		return true;
+	}
+
+	
+
+/*We are having a party with amounts of tea and candy. 
+Return the int outcome of the party encoded as 0=bad, 1=good, or 2=great. A party is good (1) if both tea and candy are at least 5. 
+However, if either tea or candy is at least double the amount of the other one, the party is great (2). 
+However, in all cases, if either tea or candy is less than 5, the party is always bad (0).
+
+
+teaParty(6, 8) → 1
+teaParty(3, 8) → 0
+teaParty(20, 6) → 2*/
+	
+	public int teaParty(int tea, int candy) {
+		int candyD = candy *2;
+		int teaD = tea * 2;
+		
+		if (candy < 5 || tea < 5) {
+			return 0;
+		}
+		
+		if (candyD <= tea || teaD <= candy) {
+			return 2;
+		}
+
+		  
+//		  if (candy >= 5 && tea >= 5) {
+//			return 1;
+//		}
+		
+		return 1;
+	}
+
+/*Given a string str, if the string starts with "f" return "Fizz". 
+If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". 
+In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+
+
+			fizzString("fig") → "Fizz"
+			fizzString("dib") → "Buzz"
+			fizzString("fib") → "FizzBuzz"*/
+	
+	public String fizzString(String str) {
+		if (str.startsWith("f")) {
+			if (str.endsWith("b")) {
+				return "FizzBuzz";
+			}else return "Fizz";
+		}
+
+		if (str.endsWith("b")) {
+			return "Buzz";
+		}
+
+		return str;
+	}
+
+	
+/*
+Given an int n, return the string form of the number followed by "!". 
+So the int 6 yields "6!". Except if the number is divisible by 3 use "Fizz" instead of the number, 
+and if the number is divisible by 5 use "Buzz", and if divisible by both 3 and 5, use "FizzBuzz". 
+Note: the % "mod" operator computes the remainder after division, so 23 % 10 yields 3. 
+What will the remainder be when one number divides evenly into another? (See also: FizzBuzz Code and Introduction to Mod)
+
+
+fizzString2(1) → "1!"
+fizzString2(2) → "2!"
+fizzString2(3) → "Fizz!"*/
+	
+	public String fizzString2(int n) {
+
+		if (n % 3 == 0) {
+			if (n% 5 == 0) {
+				return "FizzBuzz!";
+			}else return "Fizz!";
+		}
+
+		if (n% 5 == 0) {
+			return "Buzz!";
+		}
+		return n + "!";
+	}
+
+
+/*Given three ints, a b c, return true if it is possible to add two of the ints to get the third.
+
+
+twoAsOne(1, 2, 3) → true
+twoAsOne(3, 1, 2) → true
+twoAsOne(3, 2, 2) → false*/
+	
+	public boolean twoAsOne(int a, int b, int c) {
+
+		if ((a + b) == c) {
+			return true;
+		}
+
+		if ((a + c) == b) {
+			return true;
+		}
+
+		if ((b + c) == a) {
+			return true;
+		}
+
+		return false;
+	}
+
+/*Given three ints, a b c, return true if b is greater than a, and c is greater than b. 
+However, with the exception that if "bOk" is true, b does not need to be greater than a.
+
+
+			inOrder(1, 2, 4, false) → true
+			inOrder(1, 2, 1, false) → false
+			inOrder(1, 1, 2, true) → true*/
+	
+	public boolean inOrder(int a, int b, int c, boolean bOk) {
+
+		if (bOk) {
+			if (b < c) {
+				return true;
+			}
+		}
+
+		if (a < b && b < c) {
+			return true;
+		}
+		return false;
+	}
+	
+/*Given three ints, a b c, return true if two or more of them have the same rightmost digit. 
+The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+
+
+			lastDigit(23, 19, 13) → true
+			lastDigit(23, 19, 12) → false
+			lastDigit(23, 19, 3) → true*/
+
+	public boolean lastDigit(int a, int b, int c) {
+		return (a%10 == b%10 || a%10 == c%10 || b%10 == c%10);
+	}
+
+/*Given three ints, a b c, return true if one of them is 10 or more less than one of the others.
+
+
+			lessBy10(1, 7, 11) → true
+			lessBy10(1, 7, 10) → false
+			lessBy10(11, 1, 7) → true*/
+	
+	public boolean lessBy10(int a, int b, int c) {
+		return (a-b >= 10 || b-c >= 10 || a-c >= 10);
+	}
+	
+/*	Return the sum of two 6-sided dice rolls, each in the range 1..6. 
+	However, if noDoubles is true, if the two dice show the same value, increment one die to the next value,
+	wrapping around to 1 if its value was 6.
+
+
+	withoutDoubles(2, 3, true) → 5
+	withoutDoubles(3, 3, true) → 7
+	withoutDoubles(3, 3, false) → 6*/
+
+	public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+		
+		if (noDoubles) {	
+			if (die1 == die2) {
+				if (die1 == 6) {
+					return 1 + die2;
+				}else return die1 + die2 + 1;
+			}
+		}
+		return die1 + die2;
+	}
+	
+
+/*Given three ints, a b c, return true if they are in strict increasing order,
+such as 2 5 11, or 5 6 7, but not 6 5 7 or 5 5 7.
+However, with the exception that if "equalOk" is true, equality is allowed, such as 5 5 7 or 5 5 5.
+
+
+inOrderEqual(2, 5, 11, false) → true
+inOrderEqual(5, 7, 6, false) → false
+inOrderEqual(5, 5, 7, true) → true*/
+
+	public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+		if (equalOk) {
+			if (a <= b && b <= c) {
+				return true;
+			}
+
+		}
+
+
+		if (a < b && b < c) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+
+/*Given two int values, return whichever value is larger. 
+However if the two values have the same remainder when divided by 5, then the return the smaller value. 
+However, in all cases, if the two values are the same, return 0. Note: the % "mod" operator computes the remainder, e.g. 7 % 5 is 2.
+
+
+maxMod5(2, 3) → 3
+maxMod5(6, 2) → 6
+maxMod5(3, 2) → 3*/
+	
+	public int maxMod5(int a, int b) {
+		if (a == b) {
+			return 0;
+		}
+		
+		if (a%5 == b%5) {
+			if (a<b) {
+				return a;
+			}else return b;
+		}
+		
+		if (a>b) {
+			return a;
+		}
+		
+		return b;
+	}
+
+
+/*You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2. 
+If they are all the value 2, the result is 10. Otherwise if they are all the same, the result is 5.
+Otherwise so long as both b and c are different from a, the result is 1. Otherwise the result is 0.
+
+
+redTicket(2, 2, 2) → 10
+redTicket(2, 2, 1) → 0
+redTicket(0, 0, 0) → 5*/
+	
+	public int redTicket(int a, int b, int c) {
+		if (a == 2 && b == 2 && c== 2) {
+			return 10;
+		}
+
+		if (a == b &&  b == c) {
+			return 5;
+		}
+
+		if (a != b && a!= c) {
+			return 1;
+		}
+		return 0;
+	}
+
+
+/*You have a green lottery ticket, with ints a, b, and c on it. 
+If the numbers are all different from each other, the result is 0. If all of the numbers are the same, the result is 20. 
+If two of the numbers are the same, the result is 10.
+
+
+greenTicket(1, 2, 3) → 0
+greenTicket(2, 2, 2) → 20
+greenTicket(1, 1, 2) → 10
+*/
+	public int greenTicket(int a, int b, int c) {
+		if (a != b && b != c && c!= a) {
+			return 0;
+		}
+
+		if (a == b &&  b == c) {
+			return 20;
+		}
+
+		if (a == b || b == c || a == c) {
+			return 10;
+		}
+		return 0;
+
+	}
+	
+/*	You have a blue lottery ticket, with ints a, b, and c on it. 
+	This makes three pairs, which we'll call ab, bc, and ac. Consider the sum of the numbers in each pair. 
+	If any pair sums to exactly 10, the result is 10. 
+	Otherwise if the ab sum is exactly 10 more than either bc or ac sums, the result is 5. Otherwise the result is 0.
+
+
+	blueTicket(9, 1, 0) → 10
+	blueTicket(9, 2, 0) → 0
+	blueTicket(6, 1, 4) → 10*/
+	
+	public int blueTicket(int a, int b, int c) {
+		if (a + b == 10 || b + c == 10 || c+a == 10) {
+			return 10;
+		}
+
+		if ((a + b)  == (b + c + 10) || (a + b) == (c+a+10)) {
+			return 5;
+		}	
+
+		return 0;
+	}
+	
+/*Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, 
+such as the 2 in 12 and 23. (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+
+
+			shareDigit(12, 23) → true
+			shareDigit(12, 43) → false
+			shareDigit(12, 44) → false*/
+	
+	public boolean shareDigit(int a, int b) {
+
+		int al = a / 10;
+		int ar = a % 10;
+		int bl = b / 10;
+		int br = b % 10;
+
+		return al == bl || al == br || ar == bl || ar == br;
+	}
+
+/*Given 2 non-negative ints, a and b, return their sum, so long as the sum has the same number of digits as a. 
+If the sum has more digits than a, just return a without b. 
+(Note: one way to compute the number of digits of a non-negative int n is to convert it to a string with String.valueOf(n) and then check the length of the string.)
+
+
+			sumLimit(2, 3) → 5
+			sumLimit(8, 3) → 8
+			sumLimit(8, 1) → 9*/
+	
+	public int sumLimit(int a, int b) {
+		if (String.valueOf(a+b).length() > String.valueOf(a).length()) {
+			return a;
+		}
+		return a+b;
+	}
+
+
+/*We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. 
+Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+
+
+specialEleven(22) → true
+specialEleven(23) → true
+specialEleven(24) → false*/
+
+	public boolean specialEleven(int n) {
+		if (n % 11 == 0 || (n - 1) % 11 == 0) {
+			return true;
+		}
+		return false;
+	}
+
 }
